8af4be1c8f4dd06657e306ee8bc061fca5793d31
lonalee2018 <lonalee2018@gmail.com>
2018-09-21 09:40:17 +0900

    DB connection2

--- index.js
+++ index.js
@@ -46,8 +46,7 @@ app.get('/books', function(req,res){
 
 //
 const insertBook =   { title: "Example000", author: "Kim", price: 200000 };
-
-router.post('/books', (insertBook, res));
+app.post('/books', (insertBook, res));
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
