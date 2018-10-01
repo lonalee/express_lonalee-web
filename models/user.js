9a5b5db9251a3a1bd3b53dff604cc42a54fd6fc1
lonalee2018 <lonalee2018@gmail.com>
2018-10-01 17:01:06 +0900

    complete routing to signup & working on POST_USERS6

--- models/user.js
+++ models/user.js
@@ -4,9 +4,7 @@ const userSchema = new mongoose.Schema(
   {
     userId: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    mobile: { type: String, required: true },
-    email: { type: String, required: true },
-    birthday: { type: String, required: true },
+    email: { type: String, required: true }
   },
   {
     timestamps: true
